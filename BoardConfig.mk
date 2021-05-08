a25f5c576dd8c7787219a832fd01db79061a1cfc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-08 16:23:22 +0200

    olivewood: Force inclusion of BoardConfigVendor.mk
    Change-Id: Id299b1917dd95b606a392d443f743a7abf5b70e8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,4 +203,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
--include vendor/xiaomi/olivewood/BoardConfigVendor.mk
+include vendor/xiaomi/olivewood/BoardConfigVendor.mk
