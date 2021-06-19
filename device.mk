f671833012de8d7c5a8371429a6e4a7e33fd3933
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-06-19 21:42:15 +0200

    olivewood: product_launched_with_p
    Change-Id: Iadfa6184ac332b48b6ed1aad767ca0ffc3ef8334

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 $(call inherit-product, vendor/xiaomi/olivewood/olivewood-vendor.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
