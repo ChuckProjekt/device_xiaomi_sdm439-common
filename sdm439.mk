dac28253aad7ad84e9de086f43de774f3177e6e2
kondors1995 <>
2021-08-26 22:20:07 +0530

    sdm439-common: configs: Import Pixel2016Exclusive
    This allows Unlimited Backup in Google Photos in any Quality
    
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- sdm439.mk
+++ sdm439.mk
@@ -169,6 +169,10 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
