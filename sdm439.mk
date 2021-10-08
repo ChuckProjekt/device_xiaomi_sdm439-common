84a283e178195d2da2718e8465c108c6d8ec3d96
Chuck439 <firmankediri123@gmail.com>
2021-10-08 10:59:23 +0000

    sdm439-common: Build vendor variant of vendor.qti.hardware.camera.device@1.0
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- sdm439.mk
+++ sdm439.mk
@@ -188,7 +188,7 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # HW crypto
 PRODUCT_PACKAGES += \
