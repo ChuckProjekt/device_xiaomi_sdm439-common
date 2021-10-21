c33b415578c7708c7b42484a3f8e82bc135918cc
Ratoriku <a1063021545@gmail.com>
2021-10-21 22:19:11 +0800

    sdm439-common: Drop TetheringOverlay
    * It is forced to use bpf in Android S. Also we have it backported in k4.4, so this overlay is totally useless now
    
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- sdm439.mk
+++ sdm439.mk
@@ -351,10 +351,6 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
