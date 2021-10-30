06d6f4380278d7544097355a1e7fab7ea4f2e0aa
Chuck439 <firmankediri123@gmail.com>
2021-10-30 23:25:13 +0000

    sdm439-common: sepolicy: Never allow neverallows
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,7 +173,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
