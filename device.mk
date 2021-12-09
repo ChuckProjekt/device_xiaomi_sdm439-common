5b183c916988e9ce25c41af210f5c2ce803e1d04
shield44 <motoecondorxt1022@gmail.com>
2021-12-09 12:54:01 +0000

    olivewood: drop lineage specific trust hal service
    Change-Id: If52765a8745b495df9995598f18e117c424fe4cb

--- device.mk
+++ device.mk
@@ -354,10 +354,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.msm8937
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
@@ -368,7 +364,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
-    
+
 # VNDK
 PRODUCT_PACKAGES += \
     libstdc++.vendor
