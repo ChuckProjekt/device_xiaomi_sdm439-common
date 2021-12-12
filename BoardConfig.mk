85450ffc563b09814d669a170c61c183bfaa8bb5
gargabian <yashask091@gmail.com>
2021-12-12 12:09:31 +0000

    olivewood: Add SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS
    Change-Id: I8765b41916d79a7ee36538f2d1b404d282a79a9d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,6 +184,7 @@ VENDOR_SECURITY_PATCH := 2021-07-05
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
