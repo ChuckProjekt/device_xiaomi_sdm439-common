2a4c06f1c609da2f810313dd00c8fb5a87ac2d49
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-01-02 22:54:02 +0800

    sdm439-common: config.fs: Add sensors.qti.{pine,olive}

--- configs/config.fs
+++ configs/config.fs
@@ -79,6 +79,18 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[vendor/bin/sensors.qti.pine]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/sensors.qti.olive]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [vendor/bin/xtwifi-client]
 mode: 0755
 user:  AID_GPS
