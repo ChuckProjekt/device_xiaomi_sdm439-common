0a880a173ce51dffb47da9de1dfc768c6801489d
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-02-14 17:51:24 +0800

    sdm439-common: libinit: Allow devices to use theirs own init

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -117,8 +117,8 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):init_xiaomi_sdm439
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_sdm439
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_sdm439
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_sdm439
 
 # Partitions
 TARGET_COPY_OUT_VENDOR := vendor
