f7f5a953c933f90a6e0877c38b14b4a3904c77be
Chuck439 <firmankediri123@gmail.com>
2022-03-26 06:00:07 +0000

    sdm439-common: Update Perf blobs from PAN_sprout
    T89571AA3-user 11 RKQ1.200928.002 00WW_3_150 release-keys
    
    * Drop energy-awareness daemon
    
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- sdm439.mk
+++ sdm439.mk
@@ -244,7 +244,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/powerhint.xml
 
 # QMI
 PRODUCT_PACKAGES += \
