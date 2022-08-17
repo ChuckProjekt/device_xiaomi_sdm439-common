e02ab93b044c6f7fe57181bf623e664d3f92b4f9
Chuck439 <firmankediri123@gmail.com>
2022-08-17 17:59:29 +0000

    Revert "olivewood: Disable the usage of ConfigStore."
    This reverts commit f0bcf7b42d1c9e64302383a340ae4c9daae9f7e9.

--- sdm439.mk
+++ sdm439.mk
@@ -148,10 +148,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
-# ConfigStore
-PRODUCT_PACKAGES += \
-    disable_configstore
-
 # Display
 PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0.vendor \
